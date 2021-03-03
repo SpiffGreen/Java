4040a44fc18e1aa3d644368fdc9ae391974699fa
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2021-03-03 13:24:39 -0500

    Did question 3

--- Java Assignment/ReverseDNS.java
+++ Java Assignment/ReverseDNS.java
@@ -1,76 +1,18 @@
-import java.util.*
-
+import java.util.*;
 
 public class ReverseDNS {
-
-    public static void main(String args[]) {
-
-      Scanner input = new Scanner(System.in);
-
-      System.out.print("Enter Domain Name: ");
-
-      String domain = input.nextLine();
-
-      String[] splitted = strSplit(domain);
-
-      String[] reversedSplit = new String[splitted.length];
-
-      for(int i = splitted.length - 1, int j = 0; i >= 0, j < splitted.length; i--, j++) {
-
-          reversedSplit[j] = splitted[i];
-
-      }
-      string final = strJoin(reversedSplit);
-
-      System.out.println("Reversed DNS: " + final);
-
+  public static void main(String args[]) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Domain Name: ");
+    String domain = input.nextLine();
+    input.close();
+    String[] tokens = domain.split("[.]+");
+    String[] reversedTokens = new String[tokens.length];
+    int count = tokens.length - 1;
+    for(String x: tokens) {
+      reversedTokens[count--] = x;
     }
-
-    
-    public static String[] strSplit(String dns) {
-
-        String[] final = new String[dns.length];
-
-        String temp = "", selector = ".";
-
-        for(String x: dns) {
-
-            if(x == selector) {
-
-                final.push(temp);
-
-            } else {
-
-                temp += x;
-
-            }
-
-        }
-
-        return final;
-
-    }
-
-    public static String strJoin(String[] arr) {
-
-        String final = "", selector = ".";
-
-        for(int i = 0; i < arr.length; i++) {
-
-            if(i != arr.length -1) {
-
-                final += arr[i] + selector;
-
-            } else {
-
-                final += arr[i];
-
-            }
-
-        }
-
-        return final;
-
-    }
-
+    String result = String.join(".", reversedTokens);
+    System.out.println("Reversed Domain Name: " + result);
+  }
 }
